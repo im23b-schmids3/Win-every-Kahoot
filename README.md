2526d993e22132c7f6801a32d9267ac7b91038fe
schmids3 <schmids3@bzz.ch>
2025-02-27 15:24:23 +0100

    switched from groqAI to openAI

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # Gewinne jedes Kahoot Spiel!
 
-Das Skript macht mit einer Tastenkombination (ctrl + alt + o) einen Screenshot, liest den Text mithilfe von Optical Character Recognition (OCR)
-aus und gibt ihn aus. Dieser Text wird an Groq AI gesendet, welcher die Antworten auswertet und die wahrscheinlichste
-richtige Antwort zurückgibt. Diese wird dann automatisch für Sie mit hilfe von pyautogui angeklickt.
+Das Skript erstellt per Tastenkombination (Ctrl + Alt + O) einen Screenshot, extrahiert den Text mithilfe von Optical
+Character Recognition (OCR) und gibt ihn aus. Anschliessend wird dieser Text an ChatGPT gesendet, das mögliche Antworten
+analysiert und die wahrscheinlich beste Lösung zurückgibt. Diese wird dann automatisch mit pyautogui angeklickt.
 
 # Wie benutze ich es?
 
 1. Alle nötigen Bibliotheken installieren.
-2. Groq API Key beanspruchen und in einem .env File speichern. -> https://groq.com/
+2. OpenAI API Key beanspruchen und in einem .env File speichern. -> https://openai.com/index/openai-api/
 3. Space ocr API Key beanspruchen und in einem .env File speichern. -> https://ocr.space/OCRAPI
 4. Das Skript ausführen.
 5. Einem Kahoot Spiel beitreten und mit ctrl + alt + o einen Screenshot machen.
 6. Die Antwort wird automatisch für Sie angeklickt.
-
 
 # Python-Bibliotheken
 
@@ -23,7 +22,7 @@ richtige Antwort zurückgibt. Diese wird dann automatisch für Sie mit hilfe von
 <li> pip install pyautogui </li>
 <li> pip install pillow </li>
 <li> pip install pygetwindow </li>
-<li> pip install groq </li>
+<li> pip install openai </li>
 </ul>
 
 # Hinweis
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-import groq
 import keyboard
 import pyautogui
 import requests
+from openai import OpenAI
 from PIL import ImageGrab
 from dotenv import load_dotenv
 
@@ -23,7 +23,7 @@ def ocr_with_space(image_path):
 
 
 load_dotenv()
-client = groq.Client(api_key=os.getenv("GROQ_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 print("Programm läuft... Drücke STRG + ALT + O, um einen Screenshot zu machen.")
 
@@ -54,11 +54,11 @@ while True:
             print(f"Antwort 4: {antwort4}")
 
             response = client.chat.completions.create(
-                model="mixtral-8x7b-32768",
+                model="gpt-4o-mini",
                 messages=[{"role": "user",
                            "content": f'You are playing Kahoot. The question is: "{frage}". The possible answers are: '
                                       f'{antwort1}, {antwort2}, {antwort3}, {antwort4}. If there is no . or " etc. then DONT use it'
-                                      f'You are ONLY ALLOWED to Respond with the correct answer and DON’T explain anything.'}],
+                                      f'You are ONLY ALLOWED to Respond with the correct answer and DON’T explain anything. The answers can also be ONLY numbers like 2, 5, 4, 1 for example'}],
                 max_tokens=50
             )
 
@@ -72,39 +72,38 @@ while True:
             print(f"Falsch: {falsch}")
 
             response = client.chat.completions.create(
-                model="mixtral-8x7b-32768",
+                model="gpt-4o-mini",
                 messages=[{"role": "user",
                            "content": f'You are playing Kahoot. The question is: "{frage}". '
                                       f'The possible answers are: "{wahr}" (True) and "{falsch}" (False). '
-                                      f'You are ONLY ALLOWED to Respond with "{wahr}" or "{falsch}") DON’T explain anything!.'}],
+                                      f'You are ONLY ALLOWED to Respond with "{wahr}" or "{falsch}" If there is no . or " etc. then DONT use them DON’T explain anything!.'}],
                 max_tokens=50
             )
 
         else:
             print("\nFehler: Nicht genug Zeilen erkannt.")
 
-
         if response:
             richtige_antwort = response.choices[0].message.content.strip().lower()
             print(f"\nRichtige Antwort: {richtige_antwort}")
 
         if antwort1 and richtige_antwort == antwort1.strip().lower():
-            pyautogui.moveTo(1100, 1420, duration=0.1)
+            pyautogui.moveTo(1100, 1420, duration=0.05)
             pyautogui.click()
         elif antwort2 and richtige_antwort == antwort2.strip().lower():
-            pyautogui.moveTo(1100, 1600, duration=0.1)
+            pyautogui.moveTo(1100, 1600, duration=0.05)
             pyautogui.click()
         elif antwort3 and richtige_antwort == antwort3.strip().lower():
-            pyautogui.moveTo(1900, 1420, duration=0.1)
+            pyautogui.moveTo(1900, 1420, duration=0.05)
             pyautogui.click()
         elif antwort4 and richtige_antwort == antwort4.strip().lower():
-            pyautogui.moveTo(1900, 1600, duration=0.1)
+            pyautogui.moveTo(1900, 1600, duration=0.05)
             pyautogui.click()
         elif wahr and richtige_antwort == wahr.strip().lower():
-            pyautogui.moveTo(1250, 1550, duration=0.1)
+            pyautogui.moveTo(1250, 1550, duration=0.05)
             pyautogui.click()
         elif falsch and richtige_antwort == falsch.strip().lower():
-            pyautogui.moveTo(1900, 1550, duration=0.1)
+            pyautogui.moveTo(1900, 1550, duration=0.05)
             pyautogui.click()
         else:
             print("\nFehler: Keine Antwort von der KI erhalten.")
